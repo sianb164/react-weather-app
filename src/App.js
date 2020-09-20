7db816b35bb50b11541f232279c61e6a05cf0f55
Sian Burgess <sian_burgess@hotmail.co.uk>
2020-09-20 16:19:54 +0100

    Removed logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -11,5 +10,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
